79429adc0d26bd1c5df812a937d3c9497aaccd7f
RafalKalita <rafalkalita@gmail.com>
2013-08-20 15:48:23 +0100

    created clearCompleated button

--- AngularIntroduction/js/main.js
+++ AngularIntroduction/js/main.js
@@ -1,7 +1,5 @@
 function MainCtrl($scope) {
 
-	$scope.totalTodos = 4;
-
 	$scope.todos = [ {
 		text : "Learn AngularJS",
 		done : false
@@ -9,4 +7,19 @@ function MainCtrl($scope) {
 		text : "Basic tutorial for AngularJS",
 		done : false
 	} ]
+	
+	$scope.getTotalTodos = function() {
+		return $scope.todos.length;
+	}
+	
+	$scope.addTodo = function() {
+		$scope.todos.push({text:$scope.formTodoText, done:false});
+		$scope.formTodoText = '';
+	}
+	
+	$scope.clearCompleated = function() {
+		$scope.todos = _.filter($scope.todos, function(todo) {
+			return !todo.done;
+		});
+	}
 }

f8aced41b22f7d621bde7ed223a8f252039ef93e
RafalKalita <rafalkalita@gmail.com>
2013-08-20 15:04:06 +0100

    Created a list

--- AngularIntroduction/js/main.js
+++ AngularIntroduction/js/main.js
@@ -1,3 +1,12 @@
 function MainCtrl($scope) {
+
 	$scope.totalTodos = 4;
+
+	$scope.todos = [ {
+		text : "Learn AngularJS",
+		done : false
+	}, {
+		text : "Basic tutorial for AngularJS",
+		done : false
+	} ]
 }
